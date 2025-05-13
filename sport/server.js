2adae2b086b70e3e84608f9f4368d0d10f25ee39
A1rlay <113793274+A1rlay@users.noreply.github.com>
2025-05-12 20:27:14 -0600

    Update server.js
    Fixed server not running in the index.html file

--- sport/server.js
+++ sport/server.js
@@ -2,13 +2,19 @@ import express from "express";
 import cors from "cors";
 import sportsRouter from "./routes/sports.js";
 import teamsRouter from "./routes/teams.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 
 const app = express();
 const PORT = 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/sports", sportsRouter);
 app.use("/api/teams", teamsRouter);
